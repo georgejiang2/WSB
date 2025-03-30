9d9de19919dd6b99ee2010f3a31b5a76cd87eb11
Rohan Kyada <rohanjkyada04@gmail.com>
2025-03-30 01:49:42 -0400

    installed vite and made chat bot moderate installed vite and made chat bot moderate

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@ from google import genai
 # Initialize the client
 client = genai.Client(api_key="AIzaSyAJZBZN5HBWXtM5zPZiKOyee_MssBU7Htw")
 
-user_input = "Act like a wallstreetbets user. Make responses short and sweet. "
+user_input = "Act like a moderate wallstreetbets user. Make responses short and sweet. "
 # Start a conversation with the first request
 response = client.models.generate_content(model="gemini-2.0-flash", contents=user_input)
 print(response.text)
